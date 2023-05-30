147bc932d9e71129316fd6692f06e6c189fb2514
Ripley White <pearlcodes@outlook.com>
2023-05-30 07:51:12 +0100

    im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell im in hell

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./components/design";
+export * from "./components/design/atoms";
 export * from "./components/pages";
 export * from "./components/common";
 export * from "./components/tools";
