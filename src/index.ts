6395cc1504551f2bf76afa5795387233ba955ed7
Ripley White <pearlcodes@outlook.com>
2023-05-30 07:59:33 +0100

    Vite has forced my hand. I'm sorry.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 export * from "./components/design";
 export * from "./components/design/atoms";
-export * from "./components/pages";
+export * from "./components/design/media";
+export * from "./components/design/messaging";
+export * from "./components/design/navigation";
+export * from "./components/pages/";
+export * from "./components/pages/settings/account";
+export * from "./components/pages/settings/permissions";
 export * from "./components/common";
 export * from "./components/tools";
+export * from "./components/tools/form";
 
 export * from "./lib";
